3ede44f4797bd807607bac2639a32dd426a39106
rfiggs <rfiggs@hawaii.edu>
2016-10-09 14:50:14 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -10,13 +10,11 @@ if (Meteor.isClient) {
       return Tasks.find({},{sort: {createdAt: -1}})
     }
   })
+
   Template.tasks.events({
     "submit .add-task":function(event) {
       var name = event.target.name.value;
-      Tasks.insert({
-        name: name,
-        createdAt: Date()
-      });
+      Meteor.call('addTask',name);
 
       event.target.name.value = '';
 
@@ -24,7 +22,7 @@ if (Meteor.isClient) {
     },
     "click .delete-task": function (event) {
       if(confirm('Delete Task?')) {
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -35,3 +33,22 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()){
+      throw new Meteor.Error("No Access!");
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: Date(),
+      userId: Meteor.userId()
+    });
+
+  },
+  deleteTask: function(taskId){
+
+    Tasks.remove(taskId);
+  }
+});
